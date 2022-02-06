ed74f48e0c15a1145f122d7c26e1f95e36b6d45a
wealthymanyasa <omanyasa@yahoo.com>
2022-02-06 16:32:01 +0200

    added the filter by categories functionality

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -9,7 +9,9 @@ import { CartService } from 'src/app/service/cart.service';
 export class ProductsComponent implements OnInit {
 
   public productList : any;
+  public filterCategory : any;
   searchKey : string = "";
+
 
   constructor( private api : ApiService, private cartService : CartService) { }
 
@@ -17,10 +19,15 @@ export class ProductsComponent implements OnInit {
     this.api.getProduct()
     .subscribe( res =>{
       this.productList = res;
-
+      this.filterCategory = res;
+         
       this.productList.forEach((a:any)=>{
+        if(a.category === "women's clothing" || a.category === "men's clothing"){
+          a.category = "fashion";
+        }
         Object.assign(a,{quantity:1,total:a.price});
       });
+      console.log(this.productList);
     });
     this.cartService.search.subscribe((val: any) =>{
       this.searchKey = val;
@@ -29,6 +36,15 @@ export class ProductsComponent implements OnInit {
 
   addtocart(item : any){
        this.cartService.addtoCart(item);
+  }
+
+  filter(category:string){
+       this.filterCategory = this.productList
+       .filter((a:any)=>{
+         if(a.category == category || category == ''){
+           return a;
+         }
+       })
   }
 
 }
